d68a2428cf3c84961e89f828d352d07aada74a3c
wmancaniello <wmancaniello@gmail.com>
2024-03-26 16:51:06 +0100

    Finish js istruction

---
+++ README.md
@@ -0,0 +1,22 @@
+# Calcolatore del prezzo del biglietto del treno
+
+Scrivere un programma per calcolare il prezzo del biglietto del treno sulla base dei chilometri da percorrere, tenendo conto dell'età del passeggero ed eventualmente applicare lo sconto.
+
+### Istruzioni
+
+1. **Milestone 1:**
+   - [x] Implementare il programma senza alcuna estetica, utilizzando esclusivamente due input e un bottone non stilizzato.
+   - [x] Il programma deve chiedere all'utente il numero di chilometri da percorrere e l'età del passeggero.
+   - [x] Sulla base di queste informazioni, il programma calcolerà il prezzo totale del biglietto seguendo le regole:
+     - Il prezzo del biglietto è definito in base ai chilometri (0.21 € al km).
+     - Va applicato uno sconto del 20% per i minorenni.
+     - Va applicato uno sconto del 40% per gli over 65.
+     - L'output sarà visualizzato in console.
+
+2. **Milestone 2:**
+- [ ] Una volta completata e funzionante la *milestone 1*, realizzare un form in pagina:
+   - L'utente potrà inserire i dati nel form e visualizzare il calcolo finale del prezzo.
+   - Il recap dei dati e l'output del prezzo finale saranno stampati in pagina.
+   - Il prezzo deve essere formattato con massimo due decimali, per indicare i centesimi.
+
+### continuo..
--- js/script.js
+++ js/script.js
@@ -1,2 +1,68 @@
 // TEST
-alert("ciao");
+// alert("ciao");
+
+// 1. Trovo bottoni nella pagina
+// Genera
+const genBtn = document.getElementById("generate-btn"); // object | null
+console.log(genBtn, typeof genBtn);
+// Annulla
+const stopBtn = document.getElementById("stop-btn"); // object | null
+console.log(stopBtn, typeof stopBtn);
+
+// 2. Aggiunta EventListener ai btn
+
+// Annulla
+// stopBtn.addEventListener("click", function() {}
+
+// Genera
+// Stampa click del bottone
+genBtn.addEventListener("click", function () {
+  // test
+  // console.log("CLICK");
+
+  // Raccolta dei tipi di dato:
+
+  // Nome e cognome
+  const nameSurnameInput = document.getElementById("name-surname"); // object | null
+  console.log(nameSurnameInput, typeof nameSurnameInput);
+
+  const nameSurnameValue = nameSurnameInput.value; // string
+  console.log(nameSurnameValue, typeof nameSurnameValue);
+
+  // Km da percorrere
+  const kmNumberInput = document.getElementById("km-number"); // object | null
+  console.log(kmNumberInput, typeof kmNumberInput);
+
+  const kmNumberValue = parseInt(kmNumberInput.value); // number
+  console.log(kmNumberValue, typeof kmNumberValue);
+
+  // Fascia d'età
+  const ageInput = document.getElementById("age"); // object | null
+  console.log(ageInput, typeof ageInput);
+
+  const ageValue = ageInput.value; // string
+  console.log(ageValue, typeof ageValue);
+
+  //ESECUZIONE LOGICA DEL PROGRAMMA
+
+  // Prezzo per KM
+  let priceKm = 0.21; //number
+  console.log(priceKm, typeof priceKm);
+
+  // Calcolo del prezzo totale del biglietto
+  let totalPrice = kmNumberValue * priceKm; //number
+  console.log(totalPrice, typeof totalPrice);
+
+  // Applicazione dello sconto
+  if (ageValue === "minorenne") {
+    //sconto del 20%
+    totalPrice = totalPrice * 0.8;
+  } else if (ageValue === "over65") {
+    //sconto del 40%
+    totalPrice = totalPrice * 0.6;
+  }
+  console.log(totalPrice, typeof totalPrice);
+
+  // STAMPA
+  document.getElementById("result").innerHTML = ` ${totalPrice.toFixed(2)} €`;
+});
