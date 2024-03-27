e9a915a031b85d702c14f7566961f2119df1a99e
wmancaniello <wmancaniello@gmail.com>
2024-03-27 11:02:00 +0100

    Fix js code and add comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // TEST
 // alert("ciao");
 
+// Controllo da fare a fine codice simile al validatorHTML
+// "use strict";
+
 // 1. Trovo bottoni nella pagina
 // Genera
 const genBtn = document.getElementById("generate-btn"); // object | null
@@ -12,57 +15,70 @@ console.log(stopBtn, typeof stopBtn);
 // 2. Aggiunta EventListener ai btn
 
 // Annulla
-// stopBtn.addEventListener("click", function() {}
+if (stopBtn != null) {
+  stopBtn.addEventListener("click", function(){
+    // Ripulire campi
+    document.getElementById("name-surname").value = "";
+    document.getElementById("km-number").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("result").innerHTML = "";
+  })
+}
 
 // Genera
 // Stampa click del bottone
-genBtn.addEventListener("click", function () {
-  // test
-  // console.log("CLICK");
+if (genBtn !== null) {
+  genBtn.addEventListener("click", function () {
+    // test
+    // console.log("CLICK");
 
-  // Raccolta dei tipi di dato:
+    // PRELEVO I DATI:
 
-  // Nome e cognome
-  const nameSurnameInput = document.getElementById("name-surname"); // object | null
-  console.log(nameSurnameInput, typeof nameSurnameInput);
+    // Nome e cognome
+    const nameSurnameInput = document.getElementById("name-surname"); // object | null
+    console.log(nameSurnameInput, typeof nameSurnameInput);
 
-  const nameSurnameValue = nameSurnameInput.value; // string
-  console.log(nameSurnameValue, typeof nameSurnameValue);
+    // per abbreviare :
+    // constNameSurname = document.getElementById("nome").value; 
 
-  // Km da percorrere
-  const kmNumberInput = document.getElementById("km-number"); // object | null
-  console.log(kmNumberInput, typeof kmNumberInput);
+    const nameSurnameValue = nameSurnameInput.value; // string
+    console.log(nameSurnameValue, typeof nameSurnameValue);
 
-  const kmNumberValue = parseInt(kmNumberInput.value); // number
-  console.log(kmNumberValue, typeof kmNumberValue);
+    // Km da percorrere
+    const kmNumberInput = document.getElementById("km-number"); // object | null
+    console.log(kmNumberInput, typeof kmNumberInput);
 
-  // Fascia d'età
-  const ageInput = document.getElementById("age"); // object | null
-  console.log(ageInput, typeof ageInput);
+    const kmNumberValue = parseInt(kmNumberInput.value); // number
+    console.log(kmNumberValue, typeof kmNumberValue);
 
-  const ageValue = ageInput.value; // string
-  console.log(ageValue, typeof ageValue);
+    // Fascia d'età
+    const ageInput = document.getElementById("age"); // object | null
+    console.log(ageInput, typeof ageInput);
 
-  //ESECUZIONE LOGICA DEL PROGRAMMA
+    const ageValue = ageInput.value; // string
+    console.log(ageValue, typeof ageValue);
 
-  // Prezzo per KM
-  let priceKm = 0.21; //number
-  console.log(priceKm, typeof priceKm);
+    //ESECUZIONE LOGICA DEL PROGRAMMA
 
-  // Calcolo del prezzo totale del biglietto
-  let totalPrice = kmNumberValue * priceKm; //number
-  console.log(totalPrice, typeof totalPrice);
+    // Prezzo per KM
+    let priceKm = 0.21; //number
+    console.log(priceKm, typeof priceKm);
 
-  // Applicazione dello sconto
-  if (ageValue === "minorenne") {
-    //sconto del 20%
-    totalPrice = totalPrice * 0.8;
-  } else if (ageValue === "over65") {
-    //sconto del 40%
-    totalPrice = totalPrice * 0.6;
-  }
-  console.log(totalPrice, typeof totalPrice);
+    // Calcolo del prezzo totale del biglietto
+    let totalPrice = kmNumberValue * priceKm; //number
+    console.log(totalPrice, typeof totalPrice);
 
-  // STAMPA
-  document.getElementById("result").innerHTML = ` ${totalPrice.toFixed(2)} €`;
-});
+    // Applicazione dello sconto
+    if (ageValue === "minorenne") {
+      //sconto del 20%
+      totalPrice = totalPrice * 0.8;
+    } else if (ageValue === "over65") {
+      //sconto del 40%
+      totalPrice = totalPrice * 0.6;
+    }
+    console.log(totalPrice, typeof totalPrice);
+
+    // STAMPA
+    document.getElementById("result").innerHTML = ` ${totalPrice.toFixed(2)} €`;
+  });
+}
